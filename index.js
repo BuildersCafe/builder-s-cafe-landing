37974eb7ca7f96c0407b6e25be176f894e5c9970
MakerDZ <mcskb91@gmail.com>
2022-08-01 23:36:22 +0630

    linear gradient error

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ let agent = navigator.userAgent
 let splited = agent.split("(");
 let device = splited[1].charAt(0);
 const test = document.querySelector(".main_container");
-if(device == 'L'){
-    test.classList.add("android");
-}
+
 if(device == 'i'){
     test.classList.add("ios");
     document.body.style.maxHeight = "-webkit-fill-available"
