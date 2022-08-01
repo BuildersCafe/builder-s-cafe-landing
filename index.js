4b447687e58896563f6e138fb347c8f17a3a6c0f
MakerDZ <mcskb91@gmail.com>
2022-08-01 23:28:08 +0630

    testing again

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let agent = navigator.userAgent
 let splited = agent.split("(");
 let device = splited[1].charAt(0);
 const test = document.querySelector(".main_container");
-alert(device);
+if(device == 'L'){
+    test.classList.add("android");
+}
 if(device == 'i'){
     test.classList.add("ios");
-}
-if(device == 'L'){
-    
+    document.body.style.maxHeight = "-webkit-fill-available"
 }
