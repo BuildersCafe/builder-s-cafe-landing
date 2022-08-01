8956ee5d37999f43df0c7132e8bf17451af2c3ab
MakerDZ <mcskb91@gmail.com>
2022-08-01 22:48:25 +0630

    javascript testing

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 let agent = navigator.userAgent
-alert(agent);
+let data = agent.split("(");
+if(data[1].charAt(0) == 'W'){
+    alert("windows");
+}
+if(data[1].charAt(0) == 'L'){
+    alert("andriod");
+}
+if(data[1].charAt(0) == 'i'){
+    alert("ios");
+}
