0bd139cb3f64e8117f6e6a0fa8343e0a8eb156bf
MakerDZ <mcskb91@gmail.com>
2022-08-03 13:17:44 +0630

    animation function added

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ let agent = navigator.userAgent
 let splited = agent.split("(");
 let device = splited[1].charAt(0);
 const test = document.querySelector(".main_container");
+const toast = document.querySelector(".ani_container");
+let timer1, timer2;
+
+let ani_start  =() => {
+    toast.classList.add("active");
+        timer1 = setTimeout(() => {
+            toast.classList.remove("active");
+        }, 5000); //1s = 1000 milliseconds
+
+        timer2 = setTimeout(() => {
+        }, 5300);
+}
+
 
 if(device == 'i'){
     test.classList.add("ios");
